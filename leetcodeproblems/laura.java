bd370e94a1bc87505bc95b29b5c2227d836dc1e7
Sougata <skar7340@gmail.com>
2022-12-31 20:44:57 +0530

    moved to leetcodeproblems folder

--- leetcodeproblems/laura.java
+++
@@ -1,7 +0,0 @@
-package leetcodeproblems;
-
-public class laura {
-    public static void main(String[] args) {
-
-    }
-}
--- leetcodeproblems/tester.java
+++
@@ -1,7 +0,0 @@
-package leetcodeproblems;
-
-public class tester {
-    public static void main(String[] args) {
-        System.out.println("jello wo");
-    }
-}
